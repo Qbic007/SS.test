048cd79636e80535c1e55d866120e0dc9645178f
Serg Pershin <pershin@exbico.ru>
2019-09-27 17:21:15 +0500

    base acco script added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,18 @@
-var swiper = new Swiper('.slider', {
-    navigation: {
-        nextEl: '.slider__button_right',
-        prevEl: '.slider__button_left',
-    },
+$(document).ready(function () {
+    let swiper = new Swiper('.slider', {
+        navigation: {
+            nextEl: '.slider__button_right',
+            prevEl: '.slider__button_left',
+        },
+        effect: 'fade'
+    });
+    
+    let accoTitleContainer = $('.acco__title-container');
+    
+    accoTitleContainer.on('click', function () {
+        let self = $(this);
+        let accoItem = self.closest('.acco__item');
+
+        accoItem.toggleClass('active');
+    })
 });
